190e610a51d7695a02ea86b08ad7539c66bd804c
Arsenii Buriak <128735775+SenchaBka@users.noreply.github.com>
2026-01-08 21:57:51 -0500

    Deletion of unused files

--- client/src/game/index.ts
+++
@@ -1,8 +0,0 @@
-// Export all game services
-export { PlayerService } from "./PlayerService";
-export { InputService } from "./InputService";
-export { CollisionService } from "./CollisionService";
-export { GameStateService, type GameState } from "./GameStateService";
-
-// Export utilities
-export { GameUtils } from "./utils/GameUtils";
